10d95983f48ee7bcd2bdd0f3c08478943fc2c560
Braian Staimer <braian.florian@>
2021-04-11 13:57:02 -0600

    + download log

--- services/drivers/cmd.py
+++ services/drivers/cmd.py
@@ -13,12 +13,14 @@ PUBLIC_FILES_FOLDER = app.config.get('PUBLIC_FILES_FOLDER')
 
 
 def download_file(url):
+    print("download {0}".format(url))
     _filename = uuid.uuid1().hex
     _output_path = "{0}/{1}".format(PUBLIC_FILES_FOLDER, _filename,)
     filename = wget.download(url, out=_output_path)
     _file = open(_output_path, 'rb')  # opening a binary file
     _binary_file = _file.read()
     _file.close()
+    print("download finish")
     """Delete file"""
     rmfile(_output_path)
     return _binary_file
